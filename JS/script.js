384e23e0a55d555bcae89d1c6993afa618743227
dennis <dennistonye30@gmail.com>
2022-10-30 23:12:13 +0100

    add

---
+++ JS/script.js
@@ -0,0 +1,11 @@
+let sB = document.getElementById('submit');
+
+sB.addEventListener('click' , function(){
+    alert('error');
+    location.href = "https://www.w3schools.com";
+    alert('reloaded');
+    
+})
+
+
+
